6fa546b63fd6c52d5c3883f39412023e9b8032b1
김성준 <ted@bitflow.ai>
2023-04-15 21:22:11 +0900

    기초적인 SCSS 작업 분 반영

---
+++ src/components/layout/SideBar.vue
@@ -0,0 +1,257 @@
+<template>
+
+  <!--begin::Sidebar-->
+  <div id="kt_app_sidebar" class="app-sidebar flex-column" data-kt-drawer="true" data-kt-drawer-name="app-sidebar" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="auto" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_app_sidebar_mobile_toggle">
+    <!--begin::Sidebar secondary menu-->
+    <div id="kt_app_sidebar_menu" data-kt-menu="true" class="menu menu-sub-indention menu-rounded menu-column menu-active-bg menu-title-gray-600 menu-icon-gray-400 menu-state-primary menu-arrow-gray-500 fw-semibold fs-6 py-4 py-lg-6 ms-n7">
+      <div id="kt_app_sidebar_menu_wrapper" class="hover-scroll-y px-1 px-lg-5" data-kt-sticky="true" data-kt-sticky-name="app-sidebar-menu-sticky" data-kt-sticky-offset="{default: false, xl: '500px'}" data-kt-sticky-release="#kt_app_stats" data-kt-sticky-width="250px" data-kt-sticky-left="auto" data-kt-sticky-top="100px" data-kt-sticky-animation="false" data-kt-sticky-zindex="95" data-kt-scroll="true" data-kt-scroll-activate="{default: true, lg: true}" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_header, #kt_app_header" data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="20px">
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu content-->
+          <div class="menu-content">
+            <span class="menu-section fs-5 fw-bolder ps-1 py-1">Home</span>
+          </div>
+          <!--end:Menu content-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link active" href="../../demo44/dist/index.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">Default</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/ecommerce.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">eCommerce</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/projects.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">Projects</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/online-courses.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">Online Courses</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/marketing.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">Marketing</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/bidding.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">Bidding</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/pos.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">POS System</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/call-center.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">Call Center</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/logistics.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">Logistics</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/website-analytics.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">Website Analytics</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/finance-performance.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">Finance Performance</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/store-analytics.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">Store Analytics</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/social.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">Social</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/delivery.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">Delivery</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/crypto.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">Crypto</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/school.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">School</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/dashboards/podcast.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">Podcast</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+        <!--begin:Menu item-->
+        <div class="menu-item">
+          <!--begin:Menu link-->
+          <a class="menu-link" href="../../demo44/dist/landing.html">
+									<span class="menu-bullet">
+										<span class="bullet bullet-dot"></span>
+									</span>
+            <span class="menu-title">Landing</span>
+          </a>
+          <!--end:Menu link-->
+        </div>
+        <!--end:Menu item-->
+      </div>
+    </div>
+    <!--end::Sidebar secondary menu-->
+  </div>
+  <!--end::Sidebar-->
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  data () {
+    return {
+      tabidx: 0
+    }
+  },
+  methods: {
+    setActive(idx) {
+      this.tabidx = idx;
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,37 @@
-import {createWebHashHistory, createRouter} from "vue-router";
+import { createWebHashHistory, createRouter } from "vue-router";
+
+// home
+import LayoutMain from "../components/layout/LayoutMain.vue";
+import Index from "../components/Index.vue";
+
+// 네비게이션 가드
+/*
+import {
+  onlyAuthAndPtNoAndGoCharm
+} from "./navigationGuard"
+*/
 
 const routes = [
     {
-
+        path: "",
+        name: "main",
+        component: LayoutMain,
+        children: [
+            {
+                path: "main",
+                name: "main",
+                component: Index,
+            },
+            {
+                path: "",
+                component: Index,
+            },
+        ],
+    },
+    {
+        path: "",
+        name: "index",
+        component: Index,
     }
 ];
 
@@ -10,11 +39,16 @@ const router = createRouter({
     history: createWebHashHistory(),
     // base: process.env.BASE_URL,
     routes: routes,
-    scrollBehavior()
-    {
+    scrollBehavior() {
         // always scroll to top
-        return {top: 0}
+        return { top: 0 }
     },
+});
+
+router.afterEach((to, from) => {
+    const toDepth = to.path.split('/').length
+    const fromDepth = from.path.split('/').length
+    to.meta.transition = toDepth < fromDepth ? 'slide-right' : 'slide-left'
 });
 
 export default router
