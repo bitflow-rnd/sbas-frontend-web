395205f6b8d4b37bf93431312ceeab2bfc8f23bb
김성준 <ted@bitflow.ai>
2023-05-15 09:39:17 +0900

    Admin > 시스템관리 : Left 메뉴 수정 보완

--- src/components/layout/SideBar.vue
+++ src/components/layout/SideBar.vue
@@ -5,6 +5,26 @@
       <div class="depth-group">
         <div class="depth-head-box">
           <img src="/img/common/ic_lnb_organ.svg" alt="이미지">
+          <div class="head-txt-box">사용자 관리 </div>
+        </div>
+        <div class="depth-body-box">
+          <ul class="depth1">
+            <li class="depth1-box"><a href="#none" class="depth1-btn">사용자 관리</a></li>
+          </ul>
+        </div>
+        <div class="depth-head-box">
+          <img src="/img/common/ic_lnb_organ.svg" alt="이미지">
+          <div class="head-txt-box">시스템 관리 </div>
+        </div>
+        <div class="depth-body-box">
+          <ul class="depth1">
+            <li class="depth1-box"><a href="#none" class="depth1-btn">공지사항 관리</a></li>
+            <li class="depth1-box"><a href="#none" class="depth1-btn">약관/방침 관리</a></li>
+            <li class="depth1-box"><a href="#none" class="depth1-btn">App 버전 관리</a></li>
+          </ul>
+        </div>
+        <div class="depth-head-box">
+          <img src="/img/common/ic_lnb_organ.svg" alt="이미지">
           <div class="head-txt-box">기관 정보 관리 </div>
         </div>
         <div class="depth-body-box">
