e60e722fc6b03138b345ef726adee987af244311
yeonjoo86 <yearjoo086@gmail.com>
2023-06-22 17:22:23 +0900

    메인페이지 작업중

---
+++ src/components/common/MobileView.vue
@@ -0,0 +1,295 @@
+<template>
+    <div class="container fullpage">
+
+        <header class="main">
+            <div class="header-wrap">
+
+                <h1 class="logo-box">
+                    <router-link to="/comm/mobileView" ></router-link>
+                </h1>
+
+                <div class="header-menu-box">
+                    <router-link to="/login" class="header-menu-btn" >병상배정 시스템 로그인</router-link>
+                </div>
+
+
+            </div>
+        </header>
+
+        <section class="main" id="fullpage">
+
+            <section class="section section1">
+                <div class="inner wrap">
+
+                    <div class="swiper-group-box">
+
+                        <div class="swiper swiper-section1">
+
+                            <ul class="swiper-wrapper">
+
+                                <li class="swiper-slide">
+                                    <div class="item-box">
+
+                                        <div class="order-box">첫번째,</div>
+
+                                        <div class="subject-box">
+                                            코로나 19환자<br/>
+                                            <span class="accent-txt">병상배정 업무 관리</span>
+                                        </div>
+
+                                        <div class="con-box">정부기관, 지역보건소, 대학병원, 의료원, 일반병원 등과<br/>
+                                            협조체계를 구축하여 병상배정업무를 자세히 분석/관리할 수 있습니다.</div>
+
+                                    </div>
+                                </li>
+
+
+                                <li class="swiper-slide">
+                                    <div class="item-box">
+
+                                        <div class="order-box">첫번째,</div>
+
+                                        <div class="subject-box">
+                                            코로나 19환자<br/>
+                                            <span class="accent-txt">정보관리시스템 분석</span>
+                                        </div>
+
+                                        <div class="con-box">응급의료포털 E-GEN, 보건의료정보 위기대응 시스템,<br/>
+                                            보건의료자원통합신고포털, 코로나19 정보관리시스템의 정보를<br/>
+                                            확인하고, 활용할 수 있는 정보를 확인할 수 있습니다.</div>
+
+                                    </div>
+                                </li>
+
+                                <li class="swiper-slide">
+                                    <div class="item-box">
+
+                                        <div class="order-box">첫번째,</div>
+
+                                        <div class="subject-box">
+                                            코로나 19환자<br/>
+                                            <span class="accent-txt">임상정보 수집 및 표준화</span>
+                                        </div>
+
+                                        <div class="con-box">정부기관, 지역보건소, 대학병원, 의료원, 일반병원 등과<br/>
+                                            협조체계를 구축하여 병상배정업무를 자세히 분석/관리할 수 있습니다.</div>
+
+                                    </div>
+                                </li>
+                            </ul>
+
+                        </div>
+
+                        <div class="navigation-box">
+
+                            <router-link to="" class="navigation-prev-btn"></router-link>
+                            <div class="current-box">1</div>
+                            <router-link to="" class="navigation-next-btn"></router-link>
+                        </div>
+
+
+                        <div class="visual-view-box">
+                            <img src="/img/common/main_visual_item1.svg" alt="이미지">
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+            </section>
+
+            <section class="section section2">
+                <div class="inner wrap">
+
+                    <div class="swiper-group-box type-reverse">
+
+                        <div class="swiper swiper-section2">
+
+                            <ul class="swiper-wrapper">
+
+                                <li class="swiper-slide">
+                                    <div class="item-box">
+
+                                        <div class="order-box">두번째,</div>
+
+                                        <div class="subject-box">
+                                            중증도<br/>
+                                            <span class="accent-txt">분류 알고리즘</span>
+                                        </div>
+
+                                        <div class="con-box">대학병원과 협력하여 중증도 분류 알고리즘을 개발하였습니다.<br/>
+                                            학습할 데이터와 변수 검증 및 자문, 인공지능을 통해 분류된 환자의<br/>
+                                            중증도를 확인, 평가 및 피드백해드립니다.</div>
+
+                                    </div>
+                                </li>
+
+
+                                <li class="swiper-slide">
+                                    <div class="item-box">
+
+                                        <div class="order-box">두번째,</div>
+
+                                        <div class="subject-box">
+                                            병원/병상<br/>
+                                            <span class="accent-txt">추천시스템</span>
+                                        </div>
+
+                                        <div class="con-box">심플렛폼, 레몬헬스케어 등 의료전문 IT업체와 협력하여<br/>
+                                            AI관리시스템, 상황판시스템, 통합연계시스템을 개발하였습니다.<br/>
+                                            실제 임상환경에서의 활용도, 각 세부기능에 대한 중요도, 효과적인<br/>
+                                            중증도 분류 알고리즘을 탑재에 대한 평가 및 피드백을 제공합니다.<br/>
+                                            E-GEN, 병원EMR 시스템과 연동을 위해서 중앙응급의료센터 및 각 병원<br/>
+                                            실무자와 공동연구기관들의 연계를 주관하고 협업하였습니다.</div>
+
+                                    </div>
+                                </li>
+
+                            </ul>
+
+                        </div>
+
+                        <div class="navigation-box">
+
+                            <router-link to="" class="navigation-prev-btn"></router-link>
+                            <div class="current-box">1</div>
+                            <router-link to="" class="navigation-next-btn"></router-link>
+                        </div>
+
+
+                        <div class="visual-view-box">
+                            <img src="/img/common/main_visual_item2.svg" alt="이미지">
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+            </section>
+
+            <section class="section section3">
+                <div class="inner wrap">
+
+                    <div class="swiper-group-box">
+
+                        <div class="swiper swiper-section3">
+
+                            <ul class="swiper-wrapper">
+
+                                <li class="swiper-slide">
+                                    <div class="item-box">
+
+                                        <div class="order-box">세번째,</div>
+
+                                        <div class="subject-box">
+                                            다기관<br/>
+                                            <span class="accent-txt">시스템 운영</span>
+                                        </div>
+
+                                        <div class="con-box">시/도 기관, 대학교병원, 의료원, 일반병원을 통해 다양한 환경의
+                                            시범 운영을 진행하였습니다.</div>
+
+                                    </div>
+                                </li>
+
+
+                                <li class="swiper-slide">
+                                    <div class="item-box">
+
+                                        <div class="order-box">세번째,</div>
+
+                                        <div class="subject-box">
+                                            권역 네트워크를<br/>
+                                            <span class="accent-txt">이용한 시스템 운영</span>
+                                        </div>
+
+                                        <div class="con-box">시/도 기관, 감염병 공동대응 협의체 및 감염병대응 협의체를 통해<br/>
+                                            협의체 소속 의료기관에서 시스템을 운영 및 성능 평가를 진행하였습니다.</div>
+
+                                    </div>
+                                </li>
+
+                                <li class="swiper-slide">
+                                    <div class="item-box">
+
+                                        <div class="order-box">세번째,</div>
+
+                                        <div class="subject-box">
+                                            도입의향조사 및<br/>
+                                            <span class="accent-txt">실용실증위원회  개최</span>
+                                        </div>
+
+                                        <div class="con-box">확진조사서/환자 초기분류를 위한 선별 질문지의 정보를 수집하고,<br/>
+                                            각 환자의 임상정보와 경과를 수집/표준화 합니다.</div>
+
+                                    </div>
+                                </li>
+                            </ul>
+
+                        </div>
+
+                        <div class="navigation-box">
+
+                            <router-link to="" class="navigation-prev-btn"></router-link>
+                            <div class="current-box">1</div>
+                            <router-link to="" class="navigation-next-btn"></router-link>
+                        </div>
+
+
+                        <div class="visual-view-box">
+                            <img src="/img/common/main_visual_item3.svg" alt="이미지">
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+            </section>
+
+            <section class="section footer fp-auto-height">
+                <footer>
+                    <div class="inner wrap">
+                        <div class="footer-menu-box">
+
+                            <div class="footer-menu-list">
+                                <router-link to="" class="footer-menu-btn type-primary" >병상배정 시스템 로그인</router-link>
+                                <router-link to="" class="footer-menu-btn" >
+                                    <img src="/img/common/img_footer_google.svg" alt="이미지">
+                                </router-link>
+                                <router-link to="" class="footer-menu-btn">
+                                    <img src="/img/common/img_footer_apple.svg" alt="이미지">
+                                </router-link>
+                            </div>
+
+                            <router-link to="" class="footer-toggle-btn" ></router-link>
+
+
+                        </div>
+                    </div>
+                </footer>
+
+            </section>
+
+
+            <aside class="floating-box">
+                <div class="floating-img-box">
+                    <img src="/img/common/img_scroll_down.svg" alt="이미지" class="down">
+                    <img src="/img/common/img_scroll_up.svg" alt="이미지" class="up">
+                </div>
+            </aside>
+
+
+        </section>
+
+
+    </div>
+</template>
+<script setup>
+
+</script>
+<style>
+
+</style>
