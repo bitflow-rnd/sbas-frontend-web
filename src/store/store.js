6efe205b964af508ebb12c529f92b8bbd69a8e5c
yeonjoo86 <yearjoo086@gmail.com>
2023-07-18 17:52:30 +0900

    내 정보 모달 추가

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,13 @@
 import { createStore } from "vuex";
+import createPersistedState from 'vuex-persistedstate';
 import user from "../store/modules/user"
 import admin from "../store/modules/admin"
+import bedasgn from "../store/modules/bedasgn";
 const store = createStore({
     modules: {
         user,
         admin,
+        bedasgn,
     },
     state: {
     },
@@ -14,11 +17,11 @@ const store = createStore({
     },
     actions: {
     },
-    /*    plugins: [
+        plugins: [
             createPersistedState({
-                paths: ['checkup'],
+                paths: ['user'],
             })
-        ]*/
+        ]
 });
 
 export default store
