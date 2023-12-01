f4d6c97a78249f8022c75bc6b5707ff5020df7e7
yeonjoo86 <yearjoo086@gmail.com>
2023-12-01 16:56:52 +0900

    Update AppHeader.vue

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -765,7 +765,7 @@ export default {
       this.$store.dispatch('admin/getSido')
     },
     getMediList() {
-      this.$store.dispatch('admin/getMedinst', { dstrCd1: this.userInfo.dutyDstr1Cd, dstrCd2: this.userInfo.dutyDstr2Cd })
+      this.$store.dispatch('admin/getMedinst', { dstrCd1: null, dstrCd2:null })
 
     },
     showUserDetail() {
