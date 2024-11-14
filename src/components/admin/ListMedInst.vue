d6061e99e62e490431ce2a8952bf7f0b7282b68d
jiseong <jeff3877@gmail.com>
2024-11-14 14:40:50 +0900

    안쓰는 부분 정리

--- src/components/admin/ListMedInst.vue
+++ src/components/admin/ListMedInst.vue
@@ -556,11 +556,11 @@
                                 <img src="/img/common/img_profile_default.svg" alt="이미지" />
                               </td>
                               <td>{{ items.dutyDstr1Cd }}</td>
-                              <td>{{ items.ocpCd }}</td>
-                              <td>{{ items.userNm }}</td>
-                              <td>{{ items.ptTypeCdNm }}</td>
-                              <td>{{ items.jobCd }}</td>
-                              <td>{{ items.authCd }}</td>
+                              <td>{{ items.ocpCd ?? '-' }}</td>
+                              <td>{{ items.userNm ?? '-' }}</td>
+                              <td>{{ items.ptTypeCdNm ?? '-' }}</td>
+                              <td>{{ items.jobCd ?? '-' }}</td>
+                              <td>{{ items.authCd ?? '-' }}</td>
                               <td>{{ getUpDt(items.rgstDttm) }}</td>
                               <td>{{ getUpDt(items.updtDttm) }}</td>
                               <td>{{ items.userStatCdNm }}</td>
